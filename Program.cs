b972c86c12c13835b4034c7cbe99802e22aa8e13
raissajuliana <rjgalbino@uniara.edu.br>
2024-05-19 15:34:46 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RPG.src.Entities;
+
+namespace RPG 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Hero("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Clarice", 23, "Wizard");
+
+
+
+            Console.Write(hero.ToString());
+            Console.Write(hero.Attack());
+        }
+    }
+}
+
+
+
+
